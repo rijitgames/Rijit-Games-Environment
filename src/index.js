7557b00b45b12f4b322adac9cef0b4fa399ff42f
֎ Yasir Eymen Kayabaşı <93262823+yaso09@users.noreply.github.com>
2023-01-31 15:46:05 +0300

    Main code

--- src/index.js
+++ src/index.js
@@ -27,3 +27,44 @@ export{
     embed,
     getUserData
 }
+
+class rijitblocks {
+    constructor() {
+    }
+    
+    getInfo() {
+        return {
+            "id": "rijitblocks",
+            "name": "Rijit Games Blocks",
+            "blocks": [
+                        {
+                            "opcode": "getParameterByURL",
+                            "blockType": "reporter",
+                            "text": "Get parameter by url [parameter]",
+                            "arguments": {
+                                "parameter": {
+                                    "type": "string",
+                                    "defaultValue": "api"
+                                },
+                            }
+                        },
+                ],
+        };
+    }
+    
+    getParameterByURL({parameter}) {
+        this.getParameterByName = (name) => {
+            var url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+            var results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return eval(decodeURIComponent(results[2].replace(/\+/g, " ")));
+        }
+        return this.getParameterByName(parameter);
+    }
+
+}
+
+Scratch.extensions.register(new rijitblocks());
