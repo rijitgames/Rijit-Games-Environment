979aaeef11da37ebbefd94d47d88c1d738e32251
֎ Yasir Eymen Kayabaşı <93262823+yaso09@users.noreply.github.com>
2023-01-31 15:43:33 +0300

    Main of the codes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+function embed(type, html, id) {
+    if(type = "video") {
+        if(html) {
+            return "<iframe src='https://rijitgames.github.io/www/videos/" + id + ".html'></iframe>";
+        }
+        else {
+            return "https://rijitgames.github.io/www/videos" + id + ".html";
+        }
+    }
+    else if(type = "game") {
+        if(html) {
+            return "<iframe src='https://rijitgames.github.io/www/games" + id + ".html'></iframe>";
+        }
+        else {
+            return "https://rijitgames.github.io/www/games" + id + ".html";
+        }
+    }
+}
 
+function getUserData(username) {
+    return fetch("https://rijitgames.github.io/api/user/" + username).then(username2 => {
+        throw username2;
+    });
+}
+
+export{
+    embed
+}
